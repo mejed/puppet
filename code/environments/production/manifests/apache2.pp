6dc2efc94e1968be1d73fccad333dcd7a4f2919c
Majed Ghorbel <mejedgorbel@ymail.com>
2022-12-26 22:58:22 +0100

    Update apache2.pp

--- code/environments/production/manifests/apache2.pp
+++ code/environments/production/manifests/apache2.pp
@@ -10,7 +10,7 @@ $ip=$server_facts['$serverip']
 #Configuring apache2 index file
 file { "/var/www/html/index.html":
     ensure  => "present",
-    content => "The new ip is $ipaddress"
+    content => "The majed ip is $ipaddress"
     }
 
 #Starting apache2 services
